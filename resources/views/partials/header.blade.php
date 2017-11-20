22d109b9324adf06d9ed5c28abfececcbe5655fb
gFelicio <gustavo.a.felicio@gmail.com>
2017-11-20 11:29:55 -0200

    init html structure

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,31 @@
+<div class="superinfo-bg">
+    <div class="superinfo">
+        <p>
+          Seg / Sex - 08h às 18h
+        </p>
+        <a href="tel:+5516999999999">
+            +55 16 9 9999 - 9999
+        </a>
+        <p>
+            Av. Tal Tal, 120, Cidade - ESTADO
+        </p>
+    </div>
+
+    <header class="menu-bg">
+        <div class="menu">
+            <div class="menu-logo">
+                <a href="#">
+                    Flex
+                </a>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Produtos</a></li>
+                    <li><a href="#">Preço</a></li>
+                    <li><a href="#">Qualidade</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</div>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:400,400i,700" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="{!! asset('css/app.css') !!}">
+        <title>
+           Flex Blog - gFelicio
+        </title>
+    </head>
+    <body>
+        @include('partials/header')
+
+        <h1 class="introducao">
+            Novos valores e<br/>
+            Propriedades de CSS
+        </h1>
+    </body>
+</html>
